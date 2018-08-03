194f44cb859eaa0ca2df4617a8b4fb0ddea71705
henriquemalone <henrique.malone@gmail.com>
2018-08-03 13:20:13 -0300

    Initial commit

---
+++ app/src/main/java/seekbar/cursoandroid/com/seekbarseries/MainActivity.java
@@ -0,0 +1,88 @@
+package seekbar.cursoandroid.com.seekbarseries;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.SeekBar;
+
+public class MainActivity extends AppCompatActivity {
+
+    private SeekBar seekbar;
+    private ImageView imagemGosto;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        seekbar = (SeekBar) findViewById(R.id.seekbarId);
+        imagemGosto = (ImageView) findViewById(R.id.imagemId);
+
+        //imagemGosto.setVisibility(View.INVISIBLE);
+
+        seekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                //imagemGosto.setVisibility(View.VISIBLE);
+                if(i == 0){
+                    imagemGosto.setImageResource(R.drawable.pouco);
+                } else if(i == 1){
+                    imagemGosto.setImageResource(R.drawable.medio);
+                } else if(i == 2){
+                    imagemGosto.setImageResource(R.drawable.muito);
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+    }
+
+    @Override
+    public void recreate() {
+        super.recreate();
+    }
+
+    @Override
+    public void finish() {
+        super.finish();
+    }
+}
